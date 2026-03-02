bb1372fbe893081f8cc06beaa6dc9dd76a53d22d
Ayush Raj <ping2r3j@gmail.com>
2026-03-02 19:02:17 +0530

    🍺 Add terminal-games formula v0.1.0

---
+++ Formula/terminal-games.rb
@@ -0,0 +1,29 @@
+class TerminalGames < Formula
+  include Language::Python::Virtualenv
+
+  desc "A collection of classic mini games you can play right in your terminal"
+  homepage "https://github.com/ayu5h-raj/terminal-games"
+  url "https://files.pythonhosted.org/packages/cf/6f/2407e6d7810f36d9f3c5f0f578b2dbc4e899ab904ddc6d136a370647c001/terminal_games-0.1.0.tar.gz"
+  sha256 "8a26b846e1d511e40d33184c18fa4a0fdf7c4b4da7ca77ac7ba0a3d5ce056452"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  resource "blessed" do
+    url "https://files.pythonhosted.org/packages/e6/0c/658dea9ba35fcea19e6feaa8ba0d2dbf8cac9aeaa1f9ab1d77d36f534757/blessed-1.32.0.tar.gz"
+    sha256 "d4090e9908cf86bea15a5275845c8bfc69c4c34eb6d22de07c65d26f1e54a918"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/35/a2/8e3becb46433538a38726c948d3399905a4c7cabd0df578ede5dc51f0ec2/wcwidth-0.6.0.tar.gz"
+    sha256 "cdc4e4262d6ef9a1a57e018384cbeb1208d8abbc64176027e2c2455c81313159"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "terminal_games", shell_output("#{bin}/terminal-games --help 2>&1", 2)
+  end
+end
